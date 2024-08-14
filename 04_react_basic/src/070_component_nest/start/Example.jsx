5f77d9505d129a9fcd7f2d58d2c8873606c58fb2
半田優太 <yuta.h5221@gmail.com>
2024-08-14 15:48:10 +0900

    react-app-start

--- 04_react_basic/src/070_component_nest/start/Example.jsx
+++ 04_react_basic/src/070_component_nest/start/Example.jsx
@@ -1,10 +1,8 @@
-import "./Example.css";
+import Child from "./components/Child"
 
 const Example = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+      <Child />
   );
 };
 
