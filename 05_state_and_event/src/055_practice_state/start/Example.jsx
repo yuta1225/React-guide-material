d829381ba0209a130e87f59eabec3de74fa6241e
半田優太 <yuta.h5221@gmail.com>
2024-08-21 09:49:15 +0900

    react-state-practice

--- 05_state_and_event/src/055_practice_state/start/Example.jsx
+++ 05_state_and_event/src/055_practice_state/start/Example.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 const Example = () => {
+  const [ count, countSet ] = useState(0);
+  const countUp = () => {
+    countSet(count + 1);
+  }
+  const countDown = () => {
+    countSet(count - 1);
+  }
   return (
     <>
       <h3>練習問題</h3>
-      <p>
-        記述を変更し、完成コードのように+と-ボタンをクリックすると現在のカウント数が1ずつ増減する機能を実装してください。*useStateを用いてcountとsetCountを定義してください。
-      </p>
-      <p>現在のカウント数: {/* ここにcountを表示してください。*/}</p>
-      <button>+</button>
-      <button>-</button>
+      <p>現在のカウント数: {count}</p>
+      <button
+        onClick={countUp}  
+      >+</button>
+      <button
+        onClick={countDown}
+      >-</button>
     </>
   );
 };
